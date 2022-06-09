50aedb4370ee641dcd2ef504559e6cba19c2d237
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-09 15:45:54 +0300

    Image gallery

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,7 @@
+import s from '../styles.module.css';
+const ImageGallery = () => {
+  <ul className={s.ImageGallery}>
+    {/* <!-- Набор <li> с изображениями --> */}
+  </ul>;
+};
+export default ImageGallery;
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,7 @@
+import s from '../styles.module.css';
+const ImageGalleryItem = () => {
+  <li className={s.ImageGalleryItem}>
+    <img className={s.ImageGalleryItemImage} src="" alt="" />
+  </li>;
+};
+export default ImageGalleryItem;
