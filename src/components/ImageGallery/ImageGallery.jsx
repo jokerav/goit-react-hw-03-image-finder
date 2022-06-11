6f119e513d938f505987334fe0553f7bc1ffc9d6
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-11 17:59:02 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,9 @@
 import { Component } from 'react';
+import { PureComponent } from 'react';
 import s from '../styles.module.css';
 import axios from 'axios';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-class ImageGallery extends Component {
+class ImageGallery extends PureComponent {
   state = {
     status: 'idle',
     page: 1,
@@ -14,29 +15,47 @@ class ImageGallery extends Component {
     const searchRequest = `https://pixabay.com/api/?q=${querry}&page=${page}&key=25937561-4be56ebc67dabae3f5d5abc9c&image_type=photo&orientation=horizontal&per_page=12`;
     try {
       const response = await axios.get(searchRequest);
-      // this.setState(prevState => (prevState.page += 1));
+
       return response.data;
     } catch (error) {
       console.log(error.toJSON());
       return error.toJSON();
     }
   }
-  async componentDidUpdate(prevProps) {
-    const prevReuest = prevProps.request;
-    const nextRequst = this.props.request;
-    if (prevReuest !== nextRequst && nextRequst !== '') {
-      this.setState({ status: 'pending' });
+
+  async componentDidUpdate(prevProps, prevState) {
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+    const prevRequest = prevProps.request;
+    const nextRequest = this.props.request;
+
+    console.log(prevPage, nextPage);
+    console.log(prevRequest, nextRequest);
+
+    if (prevPage !== nextPage || prevRequest !== nextRequest) {
       this.getImages().then(responce => {
+        this.setState({ status: 'pending' });
         let images = [];
         responce.hits.forEach(image => {
           const { id, webformatURL, largeImageURL } = image;
           const img = { id, webformatURL, largeImageURL };
           images.push(img);
         });
-        this.setState({ images, status: 'resolved' });
+        const newState = [...prevState.images, ...images];
+
+        this.setState({ images: newState, status: 'resolved' });
+
+        // this.setState(prevState => prevState.images.push(images));
       });
     }
   }
+  onOladMore = e => {
+    // e.preventDefault();
+    this.setState(prevState => {
+      prevState.page += 1;
+    });
+    console.log(this.state.page);
+  };
   render() {
     const { status } = this.state;
     if (status === 'idle') {
@@ -65,7 +84,9 @@ class ImageGallery extends Component {
             })}
           </ul>
           {this.state.images !== [] && (
-            <button className={s.Button}>Load more</button>
+            <button onClick={this.onOladMore} className={s.Button}>
+              Load more
+            </button>
           )}
         </div>
       );
